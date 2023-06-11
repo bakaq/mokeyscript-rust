b671878ad22bc5b068d091bc49e6e3cdf2dd59c2
kaue <kaue.bazilli@usp.br>
2023-06-11 06:40:13 -0300

    Flake and envrc

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem(system:
+      let 
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in
+      with pkgs;
+      {
+        packages.default = stdenv.mkDerivation {
+          name = "monkeyscript-rust";
+          src = self;
+          buildInputs = [
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" "rust-analyzer" ];
+            })
+          ];
+        };
+      }
+    );
+}

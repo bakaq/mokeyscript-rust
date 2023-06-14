4667b44695b25b483c4e11ec8618042dba730bb0
kaue <kaue.bazilli@usp.br>
2023-06-14 08:58:42 -0300

    Improve flake.nix

--- flake.nix
+++ flake.nix
@@ -1,26 +1,51 @@
 {
+  description = "Monkeyscript interpreter writen in Rust";
+
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, flake-utils, rust-overlay }:
-    flake-utils.lib.eachDefaultSystem(system:
+    let 
+      meta = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package;
+      inherit (meta) name version;
+      overlays = [ 
+        (import rust-overlay)
+        (self: super: {
+          rustToolchain = super.rust-bin.stable.latest.default.override {
+            extensions = [ "rust-src" "rust-analyzer" "llvm-tools-preview" ];
+          };
+        })
+      ];
+    in flake-utils.lib.eachDefaultSystem(system:
       let 
-        overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
       in
-      with pkgs;
       {
-        packages.default = stdenv.mkDerivation {
-          name = "monkeyscript-rust";
-          src = self;
-          buildInputs = [
-            (rust-bin.stable.latest.default.override {
-              extensions = [ "rust-src" "rust-analyzer" "llvm-tools-preview" ];
-            })
-          ];
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              rustToolchain
+            ];
+          };
+        };
+
+        packages = rec {
+          default = monkeyscript-rust;
+          monkeyscript-rust = pkgs.rustPlatform.buildRustPackage rec {
+            pname = name;
+            inherit version;
+            src = ./.;
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+            release = true;
+          };
         };
       }
     );
